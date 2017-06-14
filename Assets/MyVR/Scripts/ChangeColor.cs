dd9bd9e50c9bbba1e6724b5c5e9eff378dc56c4c
topjohnwu <topjohnwu@gmail.com>
2017-06-14 22:53:44 +0800

    Add whole project

---
+++ Assets/MyVR/Scripts/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+	// Use this for initialization
+	void Start () {
+		setGazedAt (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setGazedAt(bool gazedAt) {
+//		GetComponent<Renderer> ().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+	}
+}
---
+++ Assets/MyVR/Scripts/SapphiAction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SapphiAction : MonoBehaviour {
+
+	public Animator _SapphiArtChanAnimator;
+
+	// Use this for initialization
+	void Start () {
+		SetAllAnimationFlagsToFalse ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetAllAnimationFlagsToFalse() {
+		_SapphiArtChanAnimator.SetBool("param_idletowalk", false);
+		_SapphiArtChanAnimator.SetBool("param_idletorunning", false);
+		_SapphiArtChanAnimator.SetBool("param_idletojump", false);
+		_SapphiArtChanAnimator.SetBool("param_idletowinpose", false);
+		_SapphiArtChanAnimator.SetBool("param_idletoko_big", false);
+		_SapphiArtChanAnimator.SetBool("param_idletodamage", false);
+		_SapphiArtChanAnimator.SetBool("param_idletohit01", false);
+		_SapphiArtChanAnimator.SetBool("param_idletohit02", false);
+		_SapphiArtChanAnimator.SetBool("param_idletohit03", false);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		SetAllAnimationFlagsToFalse ();
+		_SapphiArtChanAnimator.SetBool("param_idletowinpose", gazedAt);
+	}
+
+	public void SetTouched() {
+		SetAllAnimationFlagsToFalse ();
+		_SapphiArtChanAnimator.SetBool("param_idletodamage", true);
+	}
+}
